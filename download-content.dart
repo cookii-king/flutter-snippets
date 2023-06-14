34a022ac4f4453bd352c604344384d4fc842dcdf
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-06-14 17:33:53 +0100

    Update download-content.dart

--- download-content.dart
+++ download-content.dart
@@ -25,6 +25,38 @@
     } else {}
   }
 
+downloadFirestoreDocument(String docPath, String filePath) async {
+  if (kIsWeb) {
+    // Fetch the document from Firestore
+    final docRef = FirebaseFirestore.instance.doc(docPath);
+    final docSnapshot = await docRef.get();
+
+    if (docSnapshot.exists) {
+      // Convert the document data to JSON
+      final docData = docSnapshot.data();
+      final jsonData = jsonEncode(docData);
+
+      // Create a Blob from the JSON data
+      final blob = html.Blob([jsonData], 'application/json');
+
+      // Create a URL for the Blob
+      final url = html.Url.createObjectUrlFromBlob(blob);
+
+      // Create an anchor element and simulate a click to download the file
+      final anchor = html.AnchorElement(href: url)
+        ..setAttribute('download', filePath)
+        ..click();
+
+      // Revoke the Blob URL
+      html.Url.revokeObjectUrl(url);
+
+      print('File downloaded: $filePath');
+    } else {
+      print('Document does not exist');
+    }
+  }
+}
+
   ```ElevatedButton(
     onPressed: () async {
       try {
